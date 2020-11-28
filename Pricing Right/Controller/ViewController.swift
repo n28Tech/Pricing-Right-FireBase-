80a168c59cfa9e3af9f8b33fb88d7ed36a6f32f7
Kevin Campbell <N28Tech@icloud.com>
2020-11-28 18:56:19 -0500

    Add functionality: Button on Keyboard, hide business information form form login on screen for returning users, shows business information form, if new user is creating account for the first time.

--- Pricing Right/Controller/ViewController.swift
+++ Pricing Right/Controller/ViewController.swift
@@ -7,11 +7,29 @@
 
 import UIKit
 import Firebase
+import ButtonOnKeyboard
 
 
 
 
 class ViewController: UIViewController {
+    @IBOutlet weak var totalStackView: UIStackView!
+    
+    @IBOutlet weak var loginStackView: UIStackView!
+    
+    @IBOutlet weak var informationStackView: UIStackView!
+    
+    @IBOutlet weak var testView: UIView!
+    
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var button: UIButton!
+    
+    @IBOutlet weak var buttonHeightConstraint: NSLayoutConstraint!
+    
+    
+    
+    
+    
     
     @IBOutlet weak var emailEnterTextField: UITextField!
     
@@ -36,6 +54,9 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        informationStackView.isHidden = true
+        button.isHidden = true
     
         if Auth.auth().currentUser != nil {
                 performSegue(withIdentifier: cardSegue, sender: self)
@@ -44,6 +65,55 @@ class ViewController: UIViewController {
             }
         
     }
+    
+    
+    @IBAction func showCreateUserForm(_ sender: UIButton) {
+        loginStackView.isHidden = true
+        informationStackView.isHidden = false
+        button.isHidden = false
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(_:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(_:)), name: UIResponder.keyboardWillHideNotification, object: nil)
+        
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(hideKeyboard))
+        tapGesture.cancelsTouchesInView = false
+        view.addGestureRecognizer(tapGesture)
+        
+        button.bk_defaultButtonHeight = buttonHeightConstraint.constant  // Stores the default size of the button.
+        
+        
+    }
+    @objc func hideKeyboard() {
+        self.view.endEditing(true)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @objc func keyboardWillShow(_ notification: Notification) {
+        var visibleHeight: CGFloat = 0
+         
+        if let userInfo = notification.userInfo {
+            if let windowFrame = UIApplication.shared.keyWindow?.frame,
+                let keyboardRect = (userInfo[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue {
+                visibleHeight = windowFrame.intersection(keyboardRect).height
+            }
+        }
+        
+        updateButtonLayout(height: visibleHeight)
+    }
+     
+    @objc func keyboardWillHide(_ notification: Notification) {
+        updateButtonLayout(height: 0)
+    }
+    
+    func updateButtonLayout(height: CGFloat) {
+        button.bk_onKeyboard(scrollView: scrollView, keyboardHeight: height)
+    }
+    
+    
+    
     
     @IBAction func signUpPressed(_ sender: Any) {
         if (UITextField.validateAll(textFields: [emailEnterTextField, passwordEnterTextField,userNameText,businessSector,businessState,businessYears,experience,gender,ethnicity])) {
